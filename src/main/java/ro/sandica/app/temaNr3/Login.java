de119bdc3aa79d661383d71e5644ba8b9ee623aa
Catalin A. Sandica <catalin.sandica@hartehanks.com>
2019-02-21 20:33:27 +0200

    First Commit, Maven Project

---
+++ src/main/java/ro/sandica/app/temaNr3/Login.java
@@ -0,0 +1,39 @@
+package ro.sandica.app.temaNr3;
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+@SuppressWarnings("restriction")
+public class Login implements HttpHandler{
+	
+	
+     public void handle(HttpExchange t) throws IOException {
+     
+         String response = "<html><title>Response</title><body><h1>Login</h1>"
+         		+ "<br><br>"
+         		+ "<form action=\"http://localhost:8001/home\">"
+         		+ "First name: <input type=\"text\" name=\"fname\"><br><br>"
+         		+ "Last name: <input type=\"text\" name=\"lname\"><br>"
+         		+ "<input type=\"submit\" value=\"Submit\">"
+         		+ "</form>"
+         		+ "<br><br>"
+         		
+         		+ "</body></html>";
+         
+         t.sendResponseHeaders(200, response.length());
+         OutputStream os = t.getResponseBody();
+         os.write(response.getBytes());
+         os.close();      
+         
+     }
+	
+	
+	public Login() {
+		super();
+	}
+}
